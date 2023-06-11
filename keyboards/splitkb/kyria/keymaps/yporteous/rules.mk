d353ef489e3ada22a399e39a9f001bf88f13d04b
Younus Porteous <younusporteous@gmail.com>
2023-06-12 00:16:37 +0100

    add new keymap

---
+++ keyboards/splitkb/kyria/keymaps/yporteous/rules.mk
@@ -0,0 +1,7 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306      # Enables the use of OLED displays
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = yes     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+MOUSEKEY_ENABLE = no
+CONVERT_TO=liatris
