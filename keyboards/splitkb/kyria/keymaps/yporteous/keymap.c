4f6b7600ed3753f24a4c27c96f7ef027874ed65f
Younus Porteous <younusporteous@gmail.com>
2023-07-07 18:26:05 +0100

    add modifier info to display

--- keyboards/splitkb/kyria/keymaps/yporteous/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yporteous/keymap.c
@@ -65,8 +65,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY] = LAYOUT(
       KC_TAB  ,  KC_Q ,  KC_W ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,   KC_I ,   KC_O ,  KC_P , KC_BSPC,
-      CTL_ESC ,  KC_A ,  KC_S ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,   KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-      KC_LSFT ,  KC_Z ,  KC_X ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,  KC_COMM, KC_DOT,KC_SLSH, KC_RSFT,
+      KC_LCTL ,  KC_A ,  KC_S ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,   KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
+      KC_LSFT ,  KC_Z ,  KC_X ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,  KC_COMM, KC_DOT,KC_SLSH,  KC_ESC,
                                 NUMPAD , KC_LGUI, KC_LALT, KC_SPC , NUM   ,     SYM    , KC_ENT ,KC_RALT, KC_RGUI, KC_APP
     ),
 
@@ -86,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_NUM] = LAYOUT(
       _______,  KC_1  ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,                                      KC_6  ,  KC_7  ,  KC_8  ,  KC_9  ,  KC_0  , KC_BSPC,
-      _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,                                     KC_PGUP, KC_HOME,  KC_UP , KC_END , KC_DOT , XXXXXXX,
-      _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, KC_SCRL, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, _______,
+      _______, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                                     KC_PGUP, KC_HOME,  KC_UP , KC_END , KC_DOT , KC_RCTL,
+      _______, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, KC_SCRL, _______, XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_RSFT,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |        |  !   |  @   |  #   |  $   |  %   |                              |  ^   |  &   |  *   |  (   |  )   | Delete |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |  !   |  @   |  #   |  $   |  %   |                              |   -  |  =   |  |   |  \   |  #   |   `    |
+ * |        |      |      |      |      |      |                              |   -  |  =   |  |   |  \   |  #   |   `    |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |        |   \  |  :   |  ;   |  -   |  [   |  {   |      |  |      |   }  |   _  |  +   |  ,   |  .   |  ~   |   ¬    |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_SYM] = LAYOUT(
       _______ , KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL ,
-      _______ , KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC,                                     KC_MINS, KC_EQL , NU_PIPE, KC_NUBS, KC_NUHS, KC_GRAVE,
+      _______ , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     KC_MINS, KC_EQL , NU_PIPE, KC_NUBS, KC_NUHS, KC_GRAVE,
       _______ , KC_NUBS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, _______, _______, KC_RBRC, KC_UNDS, KC_PLUS, KC_COMM,  KC_DOT, KC_PIPE, KC_TILDE,
                                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
@@ -197,7 +197,13 @@ bool oled_task_user(void) {
         // clang-format on
 
         oled_write_P(qmk_logo, false);
-        oled_write_P(PSTR("Kyria rev3.1\n\n"), false);
+        oled_write_P(PSTR("Kyria rev3.1\n"), false);
+
+        const uint8_t modifiers = get_mods() | get_oneshot_mods();
+        oled_write_P(modifiers & MOD_MASK_CTRL ? PSTR("CTRL ") : PSTR("     "), false);
+        oled_write_P(modifiers & MOD_MASK_ALT ? PSTR("ALT ") : PSTR("    "), false);
+        oled_write_P(modifiers & MOD_MASK_SHIFT ? PSTR("SHIFT ") : PSTR("      "), false);
+        oled_write_P(modifiers & MOD_MASK_GUI ? PSTR("GUI\n") : PSTR("\n"), false);
 
         // Host Keyboard Layer Status
         oled_write_P(PSTR("Layer: "), false);
