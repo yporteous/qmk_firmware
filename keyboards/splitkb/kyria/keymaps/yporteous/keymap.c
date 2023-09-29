487fe5289d6e2155ab0622f0be403d8920dc8235
Younus Porteous <younusporteous@gmail.com>
2023-09-29 14:44:13 +0100

    adjust thumb keys to be closer to corne

--- keyboards/splitkb/kyria/keymaps/yporteous/keymap.c
+++ keyboards/splitkb/kyria/keymaps/yporteous/keymap.c
@@ -66,8 +66,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       KC_TAB  ,  KC_Q ,  KC_W ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,   KC_I ,   KC_O ,  KC_P , KC_BSPC,
       KC_LCTL ,  KC_A ,  KC_S ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,   KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-      KC_LSFT ,  KC_Z ,  KC_X ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,  KC_COMM, KC_DOT,KC_SLSH,  KC_ESC,
-                                NUMPAD , KC_LGUI, KC_LALT, KC_SPC , NUM   ,     SYM    , KC_ENT ,KC_RALT, KC_RGUI, KC_APP
+      KC_LSFT ,  KC_Z ,  KC_X ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M , KC_COMM, KC_DOT ,KC_SLSH,  KC_ESC,
+                                KC_LGUI, KC_LALT,    NUM , KC_SPC , NUMPAD,     NUMPAD , KC_ENT , SYM , KC_RALT, KC_RGUI
     ),
 
 /*
